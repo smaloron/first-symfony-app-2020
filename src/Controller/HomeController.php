13ab3e6b249f90d1f31776a79e2fb6095afb1c88
Sebastien Maloron <contact@smaloron.eu>
2020-04-21 14:53:58 +0200

    Annotations sur les contrôleurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,17 +4,27 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
 {
 
-    public function defaultAction(){
-        return new Response("Hello default");
+    public function defaultAction(Request $request){
+        $page = $request->query->get("page", 1);
+
+        return new Response("Hello default vous êtes sur la page $page");
     }
 
     public function hello($age, $name){
         return new Response("<h1>Hello $name vous avez $age ans</h1>");
+    }
+
+    public function add($n1, $n2){
+
+        $result = ($n1 + $n2);
+        $response = new Response("$n1 plus $n2 ça fait : $result");
+        return $response;
     }
 
 }
